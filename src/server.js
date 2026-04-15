ed483ab1fa9179a6654d5ed6af6674ec5256bf7e
RockeyDoggy <juan_vsarts@hotmail.com>
2026-04-14 23:01:18 -0300

    Initialize Express server for metadata fetching
    Set up an Express server to fetch and parse metadata from a given URL.

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.use((req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/metadata/', async (req, res) => {
+  try {
+    const {url} = req.query;
+    const result = await fetch (url);
+    const html await result.text();
+    const $ = cheerio.load(html);
+
+    res.json({
+      title: $('meta[property="og:title"]').attr('content') || $('title').text(),
+      desc: $('meta[property="og:description"]').attr('content'),
+      thumb: $('meta[property="og:image"]').attr('content'),
+      icon: $('meta[property="og:logo"]').attr('content'),
+      site: $('meta[property="og:site_name"]').attr('content') || new URL(url).hostname,
+    });
+  } catch(e){
+    res.status(500).json({error: 'Deu Ruim :('});
+  }
+});
+
+app.listen(3000, ()=> console.log('Servidor On!'))
